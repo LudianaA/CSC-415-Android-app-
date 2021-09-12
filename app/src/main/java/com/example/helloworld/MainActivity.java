c1f208292c44f25f624d92d96148efdad054ba93
LudianaA <60020563+LudianaA@users.noreply.github.com>
2021-09-12 18:48:11 -0400

    happy birthday changes

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,11 +6,12 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello World");
+        Log.e(TAG,"here is your error message");
     }
 }
